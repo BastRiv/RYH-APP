fefbf2d0e5d7edd8d34810f97fb3435e3347af88
Bastian Riveros <bastian@MacBook-Air-de-Bastian.local>
2022-12-03 02:19:36 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,40 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'new-account',
+    loadChildren: () => import('./pages/new-account/new-account.module').then( m => m.NewAccountPageModule)
+  },
+  {
+    path: 'reset-pass',
+    loadChildren: () => import('./pages/reset-pass/reset-pass.module').then( m => m.ResetPassPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/tabs/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'main-page',
+    loadChildren: () => import('./pages/tabs/main-page/main-page.module').then( m => m.MainPagePageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./pages/tabs/bookings/bookings.module').then( m => m.BookingsPageModule)
+  },
+  {
+    path: 'load-page',
+    loadChildren: () => import('./pages/load-page/load-page.module').then( m => m.LoadPagePageModule)
   },
 ];
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://0.0.0.0:8111/api/v1/'
 };
